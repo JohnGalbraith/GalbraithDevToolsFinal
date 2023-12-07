43692eadd423247794d95c3b7b2db06ebc3562be
JohnGalbraith <145801946+JohnGalbraith@users.noreply.github.com>
2023-12-07 10:32:33 -0400

    fixed navbar to top of page
    I added a container to hold the navbar that we are fixing to the top of the page, when I tried fixing the navbar itself the dropdown functionality was broken. Also I used position sticky instead of fixed as it was breaking the ability for the nav to span the entire width of the page

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
     </head>
     <body>
         <!-- Top Navbar-->
+        <div id="ceiling-glue">
         <ul class="topnav">
             <li><a class="active" href="#Link1">Test link 1</a></li>
-            <li><a href="#Link2">News</a></li>
-            <li><a href="#Link3">Contact</a></li>
-            <li class="right"><a href="#Link4">About</a></li>
+            <li><a href="#Link2">Test link 2</a></li>
+            <li><a href="#Link3">Test link 3</a></li>
+            <li class="right"><a href="#Link4">Test link 4</a></li>
             <li class="dropdown">
                 <a href="javascript:void(0)" class="dropbtn active">Dropdown</a>
                 <div class="dropdown-content">
@@ -18,6 +19,7 @@
                 </div>
             </li>
         </ul>
+        </div>
         <!--End Top Nav-->
         
         Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna. Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci. Aenean nec lorem. In porttitor. Donec laoreet nonummy augue. Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy. Fusce aliquet pede non pede. Suspendisse dapibus lorem pellentesque magna. Integer nulla. Donec blandit feugiat ligula. Donec hendrerit, felis et imperdiet euismod, purus ipsum pretium metus, in lacinia nulla nisl eget sapien.
