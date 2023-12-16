e099135fa586bd662a3f1c80f248943d50709107
JohnGalbraith <145801946+JohnGalbraith@users.noreply.github.com>
2023-12-15 23:01:46 -0400

    added page content

--- FPS-Games/index.php
+++ FPS-Games/index.php
@@ -30,7 +30,7 @@
         <!--End Top Nav-->
         
         <div class="content">
-        
+            <p>Honestly I dont really play many FPS, I play the odd thrid person shooters every now and then but honestly thats just because a lot of adventure games have shooter aspects to them. Generally I prefer a game with a good story and often I find FPS games to be much more focused on multiplayer gameplay and less about the story. That being said every now and then you run across a truely special FPS that not only has good gameplay but a great story as well.</p><br><h2>Halo Reach</h2><img src="../img/Halo-_Reach_box_art.png" height="265" width="356"><br><p>The Halo franchise is generally very well respected (we dont talk about the new games). I chose Halo Reach Specifically to talk about because it is head and sholders above the rest as far as story goes.<br>Unlike other Halo Games Reach doesnt follow the story of Master Chief but instead follows the story of Nobel Team. A group of spartans exclusive to the Halo Reach Game. Choosing a group that has no ties to the main story line allowed for much more freedom in the type of story that was able to be told. It did not need to fit the mold of a standard Master Chief Game. Not only that but it was a prequel. This lets the authors tell different aspects to the main story once again wihtout interfering with what we know about Master Chief. This meant the writers could be more bold in the story they told without worrying about upsetting long time fans of the franchise.<br>What we ended up with was a truely special story that is worth anyone to pick up.</p><img src="../img/lone%20wolf.jpg">
         </div>
 
 
