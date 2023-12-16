2d52572b8e9385c96ec2b7451d056128c904c73a
JohnGalbraith <145801946+JohnGalbraith@users.noreply.github.com>
2023-12-15 22:48:58 -0400

    added content

--- Favourite-Games/index.php
+++ Favourite-Games/index.php
@@ -30,7 +30,13 @@
         <!--End Top Nav-->
         
         <div class="content">
-        
+            <p>Games are a pretty great pass time, I would much rather be playing games right now than doing this but alas, I put this off too long and now I have to work on this website instead of playing games. I guess I will just have to put up with talking about my favourite games instead</p>
+            <h2>Duck Game</h2>
+            <p>This game is pretty great, super fast paced action platformer. its a bunch of duck that you threw a shot gun at and now they want to murder each other, why? I dont know, maybe they are fighting over bread. This game is really best played with friends but there is a decent arcade mode. Its really just for practice mode. If you dont hav friends then this game isnt for you.</p>
+            <h2>Deadbolt</h2>
+            <p>
+            If you dont have friends and were sad after the reading about the last game then dont worry I have the game for you. Deadbolt is a purely single player game so you dont need friends. Not only that but you can relate to the characters better too!<br>In Deadbolt you play as the grim reaper, youre really creepy and your only friend is a talking fire that tells you to kill zombies and things like that. Its a really fun puzzle game, you die from a single bullet you you need to figure out how you can stealth you way through missions and kill all the enemies with out getting caught. The sound track is done by the same person who did risk of rain so you know its great, Highly recommend.
+            </p>
         </div>
 
 
